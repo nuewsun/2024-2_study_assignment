84c3d44d056fd6e6b5e88810a001c13cb860c6e3
nuewsun <76214070+nuewsun@users.noreply.github.com>
2024-11-04 14:42:00 +0900

    Update 4_2_star_ans.cs

--- assignment4/4_2_star_ans.cs
+++ assignment4/4_2_star_ans.cs
@@ -10,10 +10,27 @@ namespace star
             int radius = int.Parse(Console.ReadLine());
             int size = 2 * (radius + 1);
 
-            // ---------- TODO ----------
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        double centerX = size / 2.0;
+        double centerY = size / 2.0;
 
+        double distance = Math.Sqrt(SqrDistance2D(j, i, centerX, centerY));
 
-            // --------------------
+        if (IsClose(distance, radius, 0.5) || IsClose(distance, radius / 2.0, 0.5))
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
+
         }
 
         // calculate the distance between (x1, y1) and (x2, y2)
