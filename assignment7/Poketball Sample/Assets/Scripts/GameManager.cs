6c15a27921a9c15ef2edfacc0b7c590a4f8b8824
nuewsun <sunnyy0905@gmail.com>
2024-11-25 17:04:33 +0900

    done

--- assignment7/Poketball Sample/Assets/Scripts/GameManager.cs
+++ assignment7/Poketball Sample/Assets/Scripts/GameManager.cs
@@ -26,7 +26,9 @@ public class GameManager : MonoBehaviour
     {
         // PlayerBall, CamObj, MyUIManager를 얻어온다.
         // ---------- TODO ---------- 
-        
+        PlayerBall = GameObject.Find("PlayerBall");
+        CamObj = GameObject.Find("Main Camera");
+        MyUIManager = GameObject.Find("Canvas").GetComponent<UIManager>();
         // -------------------- 
     }
 
@@ -40,7 +42,16 @@ public class GameManager : MonoBehaviour
     {
         // 좌클릭시 raycast하여 클릭 위치로 ShootBallTo 한다.
         // ---------- TODO ---------- 
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 targetPos = hit.point;
+                ShootBallTo(targetPos);
+            }
+        }
         // -------------------- 
     }
 
@@ -51,20 +62,36 @@ public class GameManager : MonoBehaviour
 
     void SetupBalls()
     {
-        // 15개의 공을 삼각형 형태로 배치한다.
-        // 가장 앞쪽 공의 위치는 StartPosition이며, 공의 Rotation은 StartRotation이다.
-        // 각 공은 RowSpacing만큼의 간격을 가진다.
-        // 각 공의 이름은 {index}이며, 아래 함수로 index에 맞는 Material을 적용시킨다.
-        // Obj.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ball_1");
-        // ---------- TODO ---------- 
-        
+            // 15개의 공을 삼각형 형태로 배치한다.
+            // 가장 앞쪽 공의 위치는 StartPosition이며, 공의 Rotation은 StartRotation이다.
+            // 각 공은 RowSpacing만큼의 간격을 가진다.
+            // 각 공의 이름은 {index}이며, 아래 함수로 index에 맞는 Material을 적용시킨다.
+            // Obj.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ball_1");
+            // ---------- TODO ---------- 
+            Vector3 BallPosition;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    int ballNum = i * (i + 1) / 2 + j + 1;
+                    BallPosition = StartPosition + new Vector3(2 * j - i, 0, -2 * i) * (BallRadius + RowSpacing / 2);
+                    GameObject obj = Instantiate(BallPrefab, BallPosition, StartRotation);
+                    obj.name = $"{ballNum}";
+                    obj.GetComponent<MeshRenderer>().material = Resources.Load<Material>($"Materials/ball_{ballNum}");
+                }
+            }
         // -------------------- 
     }
     void CamMove()
     {
         // CamObj는 PlayerBall을 CamSpeed의 속도로 따라간다.
         // ---------- TODO ---------- 
-        
+        if (CamObj && PlayerBall)
+            {
+                Vector3 targetPosition = PlayerBall.transform.position;
+                targetPosition.y = CamObj.transform.position.y;
+                CamObj.transform.position = Vector3.Lerp(CamObj.transform.position, targetPosition, Time.deltaTime * CamSpeed);
+            }
         // -------------------- 
     }
 
@@ -79,16 +106,24 @@ public class GameManager : MonoBehaviour
         // 힘은 CalcPower 함수로 계산하고, y축 방향 힘은 0으로 한다.
         // ForceMode.Impulse를 사용한다.
         // ---------- TODO ---------- 
-        
+        if (PlayerBall)
+            {
+                Rigidbody ballRigidbody = PlayerBall.GetComponent<Rigidbody>();
+                Vector3 displacement = targetPos - PlayerBall.transform.position;
+                Vector3 direction = displacement.normalized;
+                direction.y = 0;    
+                float power = CalcPower(displacement);
+                ballRigidbody.AddForce(direction * power, ForceMode.Impulse);
+            }
         // -------------------- 
     }
     
     // When ball falls
     public void Fall(string ballName)
     {
-        // "{ballName} falls"을 1초간 띄운다.
-        // ---------- TODO ---------- 
-        
+            // "{ballName} falls"을 1초간 띄운다.
+            // ---------- TODO ---------- 
+            MyUIManager.DisplayText($"{ballName} falls", 1f);
         // -------------------- 
     }
 }
